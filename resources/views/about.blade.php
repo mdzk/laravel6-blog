80e68d4521b291f661e3635f396ee62db68bf50b
Zack <mdzaky07@gmail.com>
2019-10-06 13:13:43 +0700

    layout contact & about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,8 @@
-<h1>About</h1>
-<p>Ini adalah halaman about</p>
+@extends('layouts.app')
+
+@section('content')
+<div class="starter-template">
+    <h1>About</h1>
+    <p class="lead">Ini adalah halaman about</p>
+</div>
+@endsection
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,2 +1,8 @@
-<h1>Contact</h1>
-<p>Ini adalah halaman Contact</p>
+@extends('layouts.app')
+
+@section('content')
+<div class="starter-template">
+    <h1>Contact</h1>
+    <p class="lead">Ini adalah halaman contact</p>
+  </div>
+@endsection
